e8cdbf9f7cf8d04ebf8a9613d4930e469233aa22
mehedi-hasan95 <mh.me.mehedi@gmail.com>
2023-09-05 07:50:32 +0600

    Remove the extra line from middleware

--- middleware.js
+++ middleware.js
@@ -1,7 +1,5 @@
 import { CheckCookieAuth } from "@/utility/MiddlewareUtility";
 
-CheckCookieAuth;
-
 export async function middleware(req) {
     if (req.nextUrl.pathname.startsWith("/blog")) {
         return await CheckCookieAuth(req);
